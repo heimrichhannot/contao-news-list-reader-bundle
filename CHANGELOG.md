1eceb0e2db1ae5d00ff64d5f1b39f0440b6af3f1
Dennis Patzer <d.patzer@heimrich-hannot.de>
2022-02-14 17:45:02 +0100

    see cl 1.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.2.1] - 2022-02-14
+
+- Fixed: array index issues in php 8+
+
 ## [1.2.0] - 2021-10-27
 - Added: php8 support
 
--- src/Item/NewsItemTrait.php
+++ src/Item/NewsItemTrait.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2021 Heimrich & Hannot GmbH
+ * Copyright (c) 2022 Heimrich & Hannot GmbH
  *
  * @license LGPL-3.0-or-later
  */
@@ -312,7 +312,7 @@ trait NewsItemTrait
         $template = new \stdClass();
         Controller::addEnclosuresToTemplate($template, $this->getRaw());
 
-        return $template->enclosure;
+        return $template->enclosure ?? null;
     }
 
     /**
